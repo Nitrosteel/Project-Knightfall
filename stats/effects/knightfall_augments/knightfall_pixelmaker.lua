b5ff9c8c8c684cf1f12311f61250f38904e1d65a
Lyrth <defelixanne@gmail.com>
2020-09-02 14:06:18 +0800

    Updated Pixelmaker and Lifesteal augments
     - Check if hit creature lost health and is an enemy
     - Add percentage mode to Lifesteal augment
     - Fixed Pixelmaker collection delay bug (sorry oof ;~;)
     - Lifesteal has cooldown when equipped to prevent
       exploitation through re-equipping

--- stats/effects/knightfall_augments/knightfall_pixelmaker.lua
+++ stats/effects/knightfall_augments/knightfall_pixelmaker.lua
@@ -1,21 +1,47 @@
 require "/scripts/util.lua"
 require "/scripts/status.lua"
 
+--[[--
+  * knightfall_pixelmaker.lua
+  * Augment status effect
+  *
+  * Attacks (beyond a damage threshold) will give the entity an amount of pixels.
+  *
+  * Created by Lyrthras#7199 on 08/29/20
+  * v1.1 [09/02/20] - check if attacked entity is an enemy, fixed
+  *                   bug where pickupDelay parameter was ignored.
+--]]--
+
+--[[--
+  === JSON fields ( * = required, - = optional ) ===
+  - damageThres - int:   [def 10] Accumulated damage above this will grant pixelAmount pixels.
+  - pixelAmount - int:   [def 1] amount of pixels to grant per damageThres damage.
+  - pickupDelay - float: [def 0] delay in seconds before the spawned pixels can be collected.
+  - onMonster   - bool:  [def false] spawn the pixels on the monster attacked instead.
+--]]--
+
 function init()
   script.setUpdateDelta(12)
 
   self.damageThres = config.getParameter("damageThres") or 10
-  self.pixelAmount = config.getParameter("pixelAmount") or 2
-  self.pickupDelay = config.getParameter("pickupDelay") or 0.5
+  self.pixelAmount = config.getParameter("pixelAmount") or 1
+  self.pickupDelay = config.getParameter("pickupDelay") or 0
+  self.onMonster   = config.getParameter("onMonster")  -- default nil/false
 
   self.totalDmg = 0
+end
+
+function isEnemy(notif)
+  local selfTeam = entity.damageTeam()
+  local otherTeam = world.entityDamageTeam(notif.targetEntityId)
+  return otherTeam and selfTeam.team ~= otherTeam.team and otherTeam.type == "enemy" or false
 end
 
 function updateDamage(notifications)
   local lastN
   for _, n in pairs(notifications) do
-    if n.hitType == "Hit" then
-      self.totalDmg = self.totalDmg + n.damageDealt
+    if n.hitType == "Hit" and isEnemy(n) then
+      self.totalDmg = self.totalDmg + n.healthLost
       lastN = n
     end
   end
@@ -27,15 +53,19 @@ end
 function trigger(notif)
   local pixels = self.pixelAmount * math.floor(self.totalDmg / self.damageThres)
   self.totalDmg = math.floor(self.totalDmg % self.damageThres)
-  local pos = notif.position
-  local randVel = {-10 + math.random() * 20, math.random() * 15}
+
+  local monPos = notif.position
+  local randVel = self.pickupDelay > 0 and
+      {-10 + math.random() * 20, math.random() * 15} or
+      {0, 0}
+
   world.spawnItem(
       "money",
-      mcontroller.position(),
+      self.onMonster and monPos or mcontroller.position(),
       pixels,
       {},
       randVel,
-      0.5
+      self.pickupDelay
   )
 end
 
