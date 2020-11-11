afce771fdd42c433c21c820316afc166c47a2dc5
Lyrth <defelixanne@gmail.com>
2020-11-11 14:46:16 +0800

    Implemented Cronus Titanslayer, added cooldown icon to Chronofrost augment
    - Added selfEffects field to statusonhit script, useful for displaying cooldowns

--- stats/effects/knightfall_augments/knightfall_statusonhit.lua
+++ stats/effects/knightfall_augments/knightfall_statusonhit.lua
@@ -16,12 +16,19 @@ require "/scripts/status.lua"
 --[[--
   === JSON fields ( * = required, - = optional ) ===
   - statusEffects - array[statusEffects]: [def {}] an array of status effects to apply on the enemy.
+  - selfEffects   - array[statusEffects]: [def {}] an array of status effects to apply on the player.
   - cooldown      - float: [def 0] cooldown between status applications. If > 0, only one enemy is hit.
+
+  Note: array[statusEffects] can be an array of strings or statusEffect definitions, e.g.:
+  "statusEffects" : [ "burning", "stasis", "wet" ],  or
+  "statusEffects" : [ {"effect" : "burning", "duration" : 1}, {"effect" : "wet"} ],  or a mix:
+  "statusEffects" : [ "burning", "stasis", {"effect" : "wet", "duration" : 1} ],
 --]]--
 
 function init()
   script.setUpdateDelta(12)
   self.statusEffects = config.getParameter("statusEffects", {})
+  self.selfEffects = config.getParameter("selfEffects", {})
   self.cooldown = config.getParameter("cooldown", 0)
 
   self.cooldownTimer = self.cooldown
@@ -30,16 +37,30 @@ function init()
     if self.cooldownTimer > 0 then return end
     self.cooldownTimer = self.cooldown
 
+    -- apply selfEffects
+    apply(self.selfEffects, entity.id(), entity.id())
+
     for _, n in pairs(notifications) do
       if n.hitType == "Hit" and n.healthLost > 0 then
-        for _, effect in ipairs(self.statusEffects) do
-          world.sendEntityMessage(n.targetEntityId, "applyStatusEffect", effect, nil, entity.id())
-        end
+        apply(self.statusEffects, n.targetEntityId, entity.id())
+
         -- only apply to one enemy
         if self.cooldown > 0 then return end
       end
     end
   end)
+end
+
+function apply(statusEffects, targetId, sourceId)
+  for _, effect in ipairs(statusEffects) do
+    local name = effect
+    local duration
+    if type(effect) == "table" then
+      name = effect.effect
+      duration = effect.duration
+    end
+    world.sendEntityMessage(targetId, "applyStatusEffect", name, duration, sourceId)
+  end
 end
 
 function update(dt)
