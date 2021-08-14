2aa8a81b3283218e3f6a9ecf7d52eb4bc21d088e
Nitrosteel <hevsanceda@gmail.com>
2021-08-14 13:00:55 +0800

    1.7.1.2
    - Added Ballistic, Plasma, Unstable, Prism, and Explosive elemental types.
    - Added KF Detector Node microdungeon.
    - Added Syn. Small Outpost microdungeon.
    - Added treasure pools for future KF encounters.
    - Added and changed KF's tilesets that was made by Anom.
    - Added Syndicate tilesets.
    - Added Everade Energy Drink, Medical Gauze, NitroX Bar, and Lifesteal Stim consumables that can be found on encounters.
    - Changed rarities of a few crafting items.
    - Added several Knightfall NPC types for future use in KF encounters.
    - Added Knightfall Colony Deed, Lab Bloodbank, and Lab Bookcase.
    - Changed and adjusted every Knightfall prop's tenant tags.
    - Added Knightfall Engineer and Scientist Tenant.
    - Added and changed some status effect icons.
    - Rebalanced most KF weapons to match the new elemental types.
    - Added several Engineer and Scientist headpiece variants.

---
+++ stats/effects/blockers/knightfall_bleedingblock/knightfall_bleedingblock.lua
@@ -0,0 +1,13 @@
+function init()
+   effect.addStatModifierGroup({{stat = "physicalResistance", amount = 0.25}, {stat = "bleedingImmunity", amount = 1}})
+
+   script.setUpdateDelta(0)
+end
+
+function update(dt)
+
+end
+
+function uninit()
+  
+end
