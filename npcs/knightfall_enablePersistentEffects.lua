95a0f10876ccd0f5ea858ffc056b42c51d88dced
Nitrosteel <66541187+Nitrosteel@users.noreply.github.com>
2025-02-07 09:25:04 +0800

    Update knightfall_enablePersistentEffects.lua
    - Fixed a bug that would throw an exception because for some reason status.addPersistentEffects doesn't like being in init().

--- npcs/knightfall_enablePersistentEffects.lua
+++ npcs/knightfall_enablePersistentEffects.lua
@@ -8,15 +8,20 @@
 	U W U ~ SILVER SOKOLOVA.
 --]]--
 
-local ini = init or function() end
-function init()
-	ini()
+local originalUpdate = update or function(...) end
+function update(...)
 	
-	local npcType = config.getParameter("kf-npcType")
-	local statusEffects = config.getParameter("kf-persistentStatusEffects", {})
-	local statList = config.getParameter("kf-statList", "knightfall_carrier_statlist")
+	if not knightfallActivatePersistentEffects then
+		local npcType = config.getParameter("kf-npcType")
+		local statusEffects = config.getParameter("kf-persistentStatusEffects", {})
+		local statList = config.getParameter("kf-statList", "knightfall_carrier_statlist")
 
-	if npcType and string.find(npc.npcType() , npcType) then
-		status.addPersistentEffects(statList, statusEffects)
+		if npcType and string.find(npc.npcType() , npcType) then
+			status.addPersistentEffects(statList, statusEffects)
+		end
+		
+		knightfallActivatePersistentEffects = true
 	end
+	
+	originalUpdate(...)
 end
