28cf6112893b78f3e7d157754eda010cfe8545e2
Nitrosteel <66541187+Nitrosteel@users.noreply.github.com>
2025-10-26 19:36:20 +0800

    Turret Rework Part I
    - Reworked sprites for the Chain Gun, Plasma, Barrage, and Missile Turrets. New names, descriptions, bug fixes, and polish included.
    - Reworked sprites for the short and tall turret platforms. Now includes passive lighting, and descriptions and names are reworked as well.
    - Armor Break now includes a debuff to physical resistance, making it effective against armored enemies.
    - Decreased counter resistance for all enemy types. Makes it less difficult for energy weapons to kill shielded enemies, and the same goes for kinetic weapons and armored enemies.
    - Fixed new tags being misplaced on merchant UIs.

--- stats/effects/knightfall_armorbreak/knightfall_armorbreak.lua
+++ stats/effects/knightfall_armorbreak/knightfall_armorbreak.lua
@@ -1,17 +1,27 @@
 function init()
-	animator.burstParticleEmitter("debris")
-	animator.playSound("break")
-	self.border = config.getParameter("border")
+  animator.burstParticleEmitter("debris")
+  animator.playSound("break")
+  self.border = config.getParameter("border")
 
-	if self.border then
-		effect.setParentDirectives("border="..self.border)
-	end
+  if self.border then
+    effect.setParentDirectives("border=" .. self.border)
+  end
 
-	effect.addStatModifierGroup(
-		{
-			{ stat = "protection", effectiveMultiplier = config.getParameter("protectionModifier", 0.5) }
-		}
-	)
+  local protectionModifier = config.getParameter("protectionModifier", 0.5)
+  local physicalResistanceMod = config.getParameter("physicalResistanceMod", 0)
+  local maxHealthModifier = config.getParameter("maxHealthModifier", 1.0)
+
+  local modifiers = {
+    { stat = "protection", effectiveMultiplier = protectionModifier }
+  }
+  if physicalResistanceMod ~= 0 then
+    table.insert(modifiers, { stat = "physicalResistance", amount = physicalResistanceMod })
+  end
+  if maxHealthModifier ~= 1.0 then
+    table.insert(modifiers, { stat = "maxHealth", effectiveMultiplier = maxHealthModifier })
+  end
+
+  self.modifierGroup = effect.addStatModifierGroup(modifiers)
 end
 
 function update(dt)
