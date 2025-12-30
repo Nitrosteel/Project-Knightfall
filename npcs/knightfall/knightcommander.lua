38f7dc26560cc5894baa736dbbb33f8db65ffc74
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2025-12-30 05:42:22 -0600

    Update knightcommander.lua

--- npcs/knightfall/knightcommander.lua
+++ npcs/knightfall/knightcommander.lua
@@ -22,3 +22,33 @@ function interact(args)
     tags = {}
   })
 end
+
+--Version for hooking into bmain.lua
+--[[
+local originalInit = init or function() end
+
+function init() originalInit()
+  knightfall_npcType = npc.npcType()
+  knightfall_questToGive = config.getParameter("quest")
+
+  if knightfall_npcType == "knightfall_knightcommander" then
+    if not entity.uniqueId() then
+      npc.setUniqueId(sb.makeUuid())
+    end
+
+    local originalHandleInteract = handleInteract or function() end    
+    handleInteract = function(args)
+      world.sendEntityMessage(args.sourceId, "knightfall_raidquestprompt", entity.uniqueId(), entity.id(), knightfall_questToGive)
+
+      sayToEntity({
+        dialogType = "dialog.converse",
+        dialog = nil,
+        entity = args.sourceId,
+        tags = {}
+      })
+
+      return originalHandleInteract(args) --This line optional
+    end
+  end
+end
+]]
