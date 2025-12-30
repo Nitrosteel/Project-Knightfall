8e4a3f2db7e2313db7c17348204448ad75f6873f
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2025-12-30 06:37:09 -0600

    Update knightcommander.lua

--- npcs/knightfall/knightcommander.lua
+++ npcs/knightfall/knightcommander.lua
@@ -7,13 +7,14 @@ function init()
   if not entity.uniqueId() then
     npc.setUniqueId(sb.makeUuid())
   end
-  knightfall_questToGive = config.getParameter("quest")
+  local quests = config.getParameter("quests")
+  storage.knightfall_questToGive = quests[math.random(#quests)]
 end
 
 function interact(args)
   mcontroller.controlFace(args.sourcePosition[1] > mcontroller.xPosition() and 1 or -1)
 
-  world.sendEntityMessage(args.sourceId, "knightfall_raidquestprompt", entity.uniqueId(), entity.id(), knightfall_questToGive)
+  world.sendEntityMessage(args.sourceId, "knightfall_raidquestprompt", entity.uniqueId(), entity.id(), storage.knightfall_questToGive)
 
   sayToEntity({
     dialogType = "dialog.converse",
@@ -22,33 +23,3 @@ function interact(args)
     tags = {}
   })
 end
-
---Version for hooking into bmain.lua
---[[
-local originalInit = init or function() end
-
-function init() originalInit()
-  knightfall_npcType = npc.npcType()
-  knightfall_questToGive = config.getParameter("quest")
-
-  if knightfall_npcType == "knightfall_knightcommander" then
-    if not entity.uniqueId() then
-      npc.setUniqueId(sb.makeUuid())
-    end
-
-    local originalHandleInteract = handleInteract or function() end    
-    handleInteract = function(args)
-      world.sendEntityMessage(args.sourceId, "knightfall_raidquestprompt", entity.uniqueId(), entity.id(), knightfall_questToGive)
-
-      sayToEntity({
-        dialogType = "dialog.converse",
-        dialog = nil,
-        entity = args.sourceId,
-        tags = {}
-      })
-
-      return originalHandleInteract(args) --This line optional
-    end
-  end
-end
-]]
