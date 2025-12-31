cbd6b87ff3bdb6f3c9f1fe31b2a22422e0a41796
Nitrosteel <66541187+Nitrosteel@users.noreply.github.com>
2025-12-31 12:04:32 +0800

    Firebase Mission Configurations
    Finalized all necessary configurations for the Syndicate Firebase mission.
    
    Pending fixes on quest UI text overflow.

---
+++ npcs/knightfall/knightfall_knightcommander.lua
@@ -0,0 +1,25 @@
+--By Silver Sokolova
+
+require "/scripts/actions/dialog.lua"
+
+function init()
+  npc.setInteractive(true)
+  if not entity.uniqueId() then
+    npc.setUniqueId(sb.makeUuid())
+  end
+  local quests = config.getParameter("quests")
+  storage.knightfall_questToGive = storage.knightfall_questToGive or quests[math.random(#quests)]
+end
+
+function interact(args)
+  mcontroller.controlFace(args.sourcePosition[1] > mcontroller.xPosition() and 1 or -1)
+
+  world.sendEntityMessage(args.sourceId, "knightfall_raidquestprompt", entity.uniqueId(), entity.id(), storage.knightfall_questToGive)
+
+  sayToEntity({
+    dialogType = "dialog.converse",
+    dialog = nil,
+    entity = args.sourceId,
+    tags = {}
+  })
+end
